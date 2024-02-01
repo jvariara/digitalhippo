527b84d11f5a2c524e537b0c227fd2fa51bc692d
jvariara <jvariara@gmail.com>
2024-01-31 21:48:16 -0500

    fixed urls for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'localhost',
-      'digitalhippo-production-bfac.up.railway.app'
-    ]
-  },
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "localhost",
+			},
+			{
+				protocol: "https",
+				hostname: "digitalhippo-production-bfac.up.railway.app",
+			},
+		],
+	},
 };
 
 export default nextConfig;
